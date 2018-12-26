36c8a1751b42a059fda53e2e6409664f2360b325
João Marques <djmz8818@gmail.com>
2018-12-26 02:08:44 +0000

    Atualização US22
    Estava de forma incorreta a us22 ja esta atualizada

--- resources/js/components/cashier.vue
+++ resources/js/components/cashier.vue
@@ -3,16 +3,16 @@
         <thead>
         <tr>
             <th scope="col">Table</th>
-            <th scope="col">Id Waiter</th>
+            <th scope="col">Waiter</th>
             <th scope="col">Total Price</th>
 
         </tr>
         </thead>
         <tbody>
-        <tr class="color-box" v-for="meal in meals">
-            <td>{{meal.table_number}}</td>
-            <td>{{meal.responsible_waiter_id}}</td>
-            <td>{{meal.total_price_preview}}</td>
+        <tr class="color-box" v-for="invoice in invoices">
+            <td>{{invoice.table_number}}</td>
+            <td>{{invoice.name}}</td>
+            <td>{{invoice.total_price}}</td>
 
 
         </tr>
@@ -23,21 +23,22 @@
     module.exports= {
         data: function () {
             return {
-                meals: []
+                invoices: []
             }
         },
         methods: {
 
-            getMealsNotPaid(){
+            getInvoicesNotPaid(){
                 axios.get('api/cashier').
                 then(response=>{
-                    this.meals = response.data;
-                    console.log(response.data);
+                    this.invoices = response.data.data;
+                    console.log(response.data.data);
+                    console.log(response.data.data);
                 })
             },
         },
         mounted() {
-            this.getMealsNotPaid();
+            this.getInvoicesNotPaid();
         },
         sockets: {
             },
