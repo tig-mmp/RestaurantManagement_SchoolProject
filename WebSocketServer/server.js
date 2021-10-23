7f8a125d63528bee99deec2d45cf86496d7f84d3
Tiago Pinto <tigmmp@hotmail.com>
2021-10-23 17:19:50 +0100

    socket url to environment variables

--- WebSocketServer/server.js
+++ WebSocketServer/server.js
@@ -1,150 +1,141 @@
 /*jshint esversion: 6 */
 
-var app = require('http').createServer();
+require("dotenv").config({ path: __dirname + "./../.env" });
 
-// Se tiverem problemas com "same-origin policy" deverão activar o CORS.
+var app = require("http").createServer();
+const io = require("socket.io")(app, {
+    allowEIO3: true,
+    cors: {
+        origin: process.env.CURRENT_URL,
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+});
 
-// Aqui, temos um exemplo de código que ativa o CORS (alterar o url base) 
+var LoggedUsers = require("./loggedusers.js");
 
-// var app = require('http').createServer(function(req,res){
-// Set CORS headers
-//  res.setHeader('Access-Control-Allow-Origin', 'http://---your-base-url---');
-//  res.setHeader('Access-Control-Request-Method', '*');
-//  res.setHeader('Access-Control-Allow-Methods', 'UPGRADE, OPTIONS, GET');
-//  res.setHeader('Access-Control-Allow-Credentials', true);
-//  res.setHeader('Access-Control-Allow-Headers', req.header.origin);
-//  if ( req.method === 'OPTIONS' || req.method === 'UPGRADE' ) {
-//      res.writeHead(200);
-//      res.end();
-//      return;
-//  }
-// });
-
-// NOTA: A solução correta depende da configuração do próprio servidor, 
-// e alguns casos do próprio browser.
-// Assim sendo, não se garante que a solução anterior funcione.
-// Caso não funcione é necessário procurar/investigar soluções alternativas
-
-var io = require('socket.io')(app);
-
-var LoggedUsers = require('./loggedusers.js');
-
-app.listen(8080, function(){
-    console.log('listening on *:8080');
+app.listen(8080, function() {
+    console.log("listening on *:8080");
 });
 
 // ------------------------
 // Estrutura dados - server
 // ------------------------
 
-// loggedUsers = the list (map) of logged users. 
+// loggedUsers = the list (map) of logged users.
 // Each list element has the information about the user and the socket id
 // Check loggedusers.js file
 
 let loggedUsers = new LoggedUsers();
 
-io.on('connection', function (socket) {
-    console.log('client has connected (socket ID = '+socket.id+')' );
-    
-	socket.on('user_enter', function (user) {
-	if (user !== undefined && user !== null)
-		console.log(user.name + ' joined ' + user.type);
-		socket.join(user.type);
-		loggedUsers.addUserInfo(user, socket.id);
-	});
-	socket.on('user_exit', function (user) {
-		if (user !== undefined && user !== null)
-			console.log(user.name + ' exit ' + user.type);
-			socket.leave(user.type);
-			loggedUsers.removeUserInfoByID(user.id);
-	});
+io.on("connection", function(socket) {
+    console.log("client has connected (socket ID = " + socket.id + ")");
 
-	socket.on('msg_from_worker_to_managers', function (msg, userInfo) {
-		if (userInfo !== undefined) {
-			let mensage = userInfo.name + ' (' + userInfo.type + '): "' + msg + '"';
-			io.sockets.to('manager').emit('msg_from_server_managers', mensage, userInfo);
-		}
-	});
+    socket.on("user_enter", function(user) {
+        if (user !== undefined && user !== null)
+            console.log(user.name + " joined " + user.type);
+        socket.join(user.type);
+        loggedUsers.addUserInfo(user, socket.id);
+    });
+    socket.on("user_exit", function(user) {
+        if (user !== undefined && user !== null)
+            console.log(user.name + " exit " + user.type);
+        socket.leave(user.type);
+        loggedUsers.removeUserInfoByID(user.id);
+    });
 
-	socket.on('orderAddCook', function (order) {//order mudou o estado para confirmed
-		io.sockets.to('cook').emit('cookNewOrder', order);
-	});
-	socket.on('orderRemoveWaiterPendingAddAllCook', function (orderId, destUserId) {
-		let userInfo = loggedUsers.userInfoByID(destUserId);
-		let socket_id = userInfo !== undefined ? userInfo.socketID : null;
-		if (socket_id !== null) {
-			io.to(socket_id).emit('waiterRemovePending', orderId);
-		}
-		io.sockets.to('cook').emit('cookRemoveOrder', orderId);
-	});
-	socket.on('orderAddWaiterPrepared', function (order, destUserId) {
-		let userInfo = loggedUsers.userInfoByID(destUserId);
-		let socket_id = userInfo !== undefined ? userInfo.socketID : null;
-		if (socket_id !== null) {
-			io.to(socket_id).emit('orderPrepared', order);
-		}
-	});
-	socket.on('orderRemoveAllCook', function (orderId) {
-		io.sockets.to('cook').emit('cookRemoveOrder', orderId);
-	});
-    socket.on('orderRemoveCook', function (orderId, destUserId) {
+    socket.on("msg_from_worker_to_managers", function(msg, userInfo) {
+        if (userInfo !== undefined) {
+            let mensage =
+                userInfo.name + " (" + userInfo.type + '): "' + msg + '"';
+            io.sockets
+                .to("manager")
+                .emit("msg_from_server_managers", mensage, userInfo);
+        }
+    });
+
+    socket.on("orderAddCook", function(order) {
+        //order mudou o estado para confirmed
+        io.sockets.to("cook").emit("cookNewOrder", order);
+    });
+    socket.on("orderRemoveWaiterPendingAddAllCook", function(
+        orderId,
+        destUserId
+    ) {
         let userInfo = loggedUsers.userInfoByID(destUserId);
         let socket_id = userInfo !== undefined ? userInfo.socketID : null;
         if (socket_id !== null) {
-            io.to(socket_id).emit('cookRemoveOrder', orderId);
+            io.to(socket_id).emit("waiterRemovePending", orderId);
+        }
+        io.sockets.to("cook").emit("cookRemoveOrder", orderId);
+    });
+    socket.on("orderAddWaiterPrepared", function(order, destUserId) {
+        let userInfo = loggedUsers.userInfoByID(destUserId);
+        let socket_id = userInfo !== undefined ? userInfo.socketID : null;
+        if (socket_id !== null) {
+            io.to(socket_id).emit("orderPrepared", order);
         }
     });
-	socket.on('orderAddWaiterPreparedDireto', function (order, destUserId) {
-		let userInfo = loggedUsers.userInfoByID(destUserId);
-		let socket_id = userInfo !== undefined ? userInfo.socketID : null;
-		if (socket_id !== null) {
-			io.to(socket_id).emit('waiterRemovePendingWithoutToast', order.id);
-			io.to(socket_id).emit('orderPrepared', order);
-		}
-		io.sockets.to('cook').emit('cookRemoveOrder', order.id);
-	});
+    socket.on("orderRemoveAllCook", function(orderId) {
+        io.sockets.to("cook").emit("cookRemoveOrder", orderId);
+    });
+    socket.on("orderRemoveCook", function(orderId, destUserId) {
+        let userInfo = loggedUsers.userInfoByID(destUserId);
+        let socket_id = userInfo !== undefined ? userInfo.socketID : null;
+        if (socket_id !== null) {
+            io.to(socket_id).emit("cookRemoveOrder", orderId);
+        }
+    });
+    socket.on("orderAddWaiterPreparedDireto", function(order, destUserId) {
+        let userInfo = loggedUsers.userInfoByID(destUserId);
+        let socket_id = userInfo !== undefined ? userInfo.socketID : null;
+        if (socket_id !== null) {
+            io.to(socket_id).emit("waiterRemovePendingWithoutToast", order.id);
+            io.to(socket_id).emit("orderPrepared", order);
+        }
+        io.sockets.to("cook").emit("cookRemoveOrder", order.id);
+    });
 
-	socket.on('mealCreated', function (table) {//os waiters so precisam de receber a table
-		io.sockets.to('waiter').emit('mealCreated', table);
-		io.sockets.to('manager').emit('mealCreated');
-	});
+    socket.on("mealCreated", function(table) {
+        //os waiters so precisam de receber a table
+        io.sockets.to("waiter").emit("mealCreated", table);
+        io.sockets.to("manager").emit("mealCreated");
+    });
 
-	socket.on('mealRemoved', function (table, mealId) {
-		io.sockets.to('waiter').emit('mealRemoved', table);
-		io.sockets.to('manager').emit('managerMealRemoved', mealId);
-	});
+    socket.on("mealRemoved", function(table, mealId) {
+        io.sockets.to("waiter").emit("mealRemoved", table);
+        io.sockets.to("manager").emit("managerMealRemoved", mealId);
+    });
 
-	socket.on('setAsNotPaid', function (waiterId) {
-		io.sockets.to('manager').emit('setAsNotPaid');
-		io.sockets.to('cashier').emit('updateInvoicesNotPaid');
-		io.sockets.to('cook').emit('mealTerminated');
-		let userInfo = loggedUsers.userInfoByID(waiterId);
-		let socket_id = userInfo !== undefined ? userInfo.socketID : null;
-		if (socket_id !== null) {
-			io.to(socket_id).emit('waiterUpdateOrders');
-		}
-	});
+    socket.on("setAsNotPaid", function(waiterId) {
+        io.sockets.to("manager").emit("setAsNotPaid");
+        io.sockets.to("cashier").emit("updateInvoicesNotPaid");
+        io.sockets.to("cook").emit("mealTerminated");
+        let userInfo = loggedUsers.userInfoByID(waiterId);
+        let socket_id = userInfo !== undefined ? userInfo.socketID : null;
+        if (socket_id !== null) {
+            io.to(socket_id).emit("waiterUpdateOrders");
+        }
+    });
 
-	socket.on('invoicePaid', function () {
-		io.sockets.to('manager').emit('setAsPaid');
-		io.sockets.to('cashier').emit('invoicePaid');
-	});
+    socket.on("invoicePaid", function() {
+        io.sockets.to("manager").emit("setAsPaid");
+        io.sockets.to("cashier").emit("invoicePaid");
+    });
 
-	socket.on('newInvoice', function () {
-		io.sockets.to('cashier').emit('newInvoice');
-		io.sockets.to('manager').emit('managerNewInvoice');
-	});
+    socket.on("newInvoice", function() {
+        io.sockets.to("cashier").emit("newInvoice");
+        io.sockets.to("manager").emit("managerNewInvoice");
+    });
 
-	socket.on('updateItems', function () {
-		io.sockets.to('waiter').emit('updateItems');
-	});
-	socket.on('updateTables', function () {
-		io.sockets.to('waiter').emit('updateTables');
-	});
+    socket.on("updateItems", function() {
+        io.sockets.to("waiter").emit("updateItems");
+    });
+    socket.on("updateTables", function() {
+        io.sockets.to("waiter").emit("updateTables");
+    });
 
-	socket.on('updateOrder', function (mealId) {
-		io.sockets.to('manager').emit('updateManagerOrders', mealId);
-	});
-
-
+    socket.on("updateOrder", function(mealId) {
+        io.sockets.to("manager").emit("updateManagerOrders", mealId);
+    });
 });
--- resources/js/vue.js
+++ resources/js/vue.js
@@ -6,91 +6,145 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
-window.Vue = require('vue');
+require("./bootstrap");
+window.Vue = require("vue");
 
-import VueRouter from 'vue-router';
+import VueRouter from "vue-router";
 Vue.use(VueRouter);
 
-import Toasted from 'vue-toasted';
+import Toasted from "vue-toasted";
 Vue.use(Toasted);
 
-import VueSocketio from 'vue-socket.io';
-Vue.use(new VueSocketio({
-    debug: true,
-    connection: 'http://192.168.10.10:8080' //TODO
-}));
+import VueSocketio from "vue-socket.io";
+Vue.use(
+    new VueSocketio({
+        debug: true,
+        connection: process.env.VUE_APP_WEBSOCKET_URL
+    })
+);
+console.log(process.env);
+import store from "./stores/global-store";
 
-
-import store from './stores/global-store';
-
-
-const menu = Vue.component('menu-restaurant', require('./components/menuRestaurant.vue'));
-const login = Vue.component('login', require('./components/login.vue'));
-const logout = Vue.component('logout', require('./components/logout.vue'));
-const manager = Vue.component('manager', require('./components/manager.vue'));
-const profile = Vue.component('profile', require('./components/profile.vue'));
-const editProfile = Vue.component('editProfile', require('./components/editProfile.vue'));
-const editPassword = Vue.component('editPassword', require('./components/editPassword.vue'));
-const shift = Vue.component('shift', require('./components/shift.vue'));
-const navbar = Vue.component('navbar', require('./components/navbar.vue'));
-const createUser = Vue.component('createUser', require('./components/Manager/createUser.vue'));
-const cashier = Vue.component('cashier', require('./components/cashier.vue'));
-const managerItemList = Vue.component('managerItemList', require('./components/Manager/managerItemList.vue'));
-const editItem = Vue.component('editItem', require('./components/Manager/editItem.vue'));
-const createItem = Vue.component('createItem', require('./components/Manager/createItem.vue'));
-const managerTableList = Vue.component('managerTableList', require('./components/Manager/managerTableList.vue'));
-const managerUserList = Vue.component('managerUserList', require('./components/Manager/managerUserList.vue'));
-const editUser = Vue.component('editUser', require('./components/Manager/editUser.vue'));
-const dashboard = Vue.component('dashboard', require('./components/Manager/dashboard.vue'));
-const managersNotifications = Vue.component('managersNotifications', require('./components/Manager/managersNotifications.vue'));
-
+const menu = Vue.component(
+    "menu-restaurant",
+    require("./components/menuRestaurant.vue")
+);
+const login = Vue.component("login", require("./components/login.vue"));
+const logout = Vue.component("logout", require("./components/logout.vue"));
+const manager = Vue.component("manager", require("./components/manager.vue"));
+const profile = Vue.component("profile", require("./components/profile.vue"));
+const editProfile = Vue.component(
+    "editProfile",
+    require("./components/editProfile.vue")
+);
+const editPassword = Vue.component(
+    "editPassword",
+    require("./components/editPassword.vue")
+);
+const shift = Vue.component("shift", require("./components/shift.vue"));
+const navbar = Vue.component("navbar", require("./components/navbar.vue"));
+const createUser = Vue.component(
+    "createUser",
+    require("./components/Manager/createUser.vue")
+);
+const cashier = Vue.component("cashier", require("./components/cashier.vue"));
+const managerItemList = Vue.component(
+    "managerItemList",
+    require("./components/Manager/managerItemList.vue")
+);
+const editItem = Vue.component(
+    "editItem",
+    require("./components/Manager/editItem.vue")
+);
+const createItem = Vue.component(
+    "createItem",
+    require("./components/Manager/createItem.vue")
+);
+const managerTableList = Vue.component(
+    "managerTableList",
+    require("./components/Manager/managerTableList.vue")
+);
+const managerUserList = Vue.component(
+    "managerUserList",
+    require("./components/Manager/managerUserList.vue")
+);
+const editUser = Vue.component(
+    "editUser",
+    require("./components/Manager/editUser.vue")
+);
+const dashboard = Vue.component(
+    "dashboard",
+    require("./components/Manager/dashboard.vue")
+);
+const managersNotifications = Vue.component(
+    "managersNotifications",
+    require("./components/Manager/managersNotifications.vue")
+);
 
 const routes = [
-    { path: '/', redirect: '/menu' },
-    { path: '/menu', component: menu, name: 'menu'},
-    { path: '/login', component: login, name: 'login'},
-    { path: '/logout', component: logout, name: 'logout'},
-    { path: '/manager', component: manager, name: 'manager',
-        children:[
-            { path:'managerItemList', component: managerItemList, name: 'managerItemList'},
-            { path:'editItem', component: editItem, name: 'editItem'},
-            { path:'createItem', component: createItem, name: 'createItem'},
-            { path:'managerTableList', component: managerTableList, name: 'managerTableList'},
-            { path:'managerUserList', component: managerUserList, name: 'managerUserList'},
-            { path: 'createUser', component: createUser, name: 'createUser'},
-            { path: 'editUser', component: editUser, name: 'editUser'},
-            { path: 'dashboard', component: dashboard, name: 'dashboard'},
+    { path: "/", redirect: "/menu" },
+    { path: "/menu", component: menu, name: "menu" },
+    { path: "/login", component: login, name: "login" },
+    { path: "/logout", component: logout, name: "logout" },
+    {
+        path: "/manager",
+        component: manager,
+        name: "manager",
+        children: [
+            {
+                path: "managerItemList",
+                component: managerItemList,
+                name: "managerItemList"
+            },
+            { path: "editItem", component: editItem, name: "editItem" },
+            { path: "createItem", component: createItem, name: "createItem" },
+            {
+                path: "managerTableList",
+                component: managerTableList,
+                name: "managerTableList"
+            },
+            {
+                path: "managerUserList",
+                component: managerUserList,
+                name: "managerUserList"
+            },
+            { path: "createUser", component: createUser, name: "createUser" },
+            { path: "editUser", component: editUser, name: "editUser" },
+            { path: "dashboard", component: dashboard, name: "dashboard" }
         ]
     },
-	{ path: '/profile', component: profile, name: 'profile'},
-    { path: '/editProfile', component: editProfile, name: 'editProfile'},
-    { path: '/editPassword', component: editPassword, name: 'editPassword'},
-    { path: '/shift', component: shift, name: 'shift'},
-    { path: '/cashier', component: cashier, name: 'cashier'},
+    { path: "/profile", component: profile, name: "profile" },
+    { path: "/editProfile", component: editProfile, name: "editProfile" },
+    { path: "/editPassword", component: editPassword, name: "editPassword" },
+    { path: "/shift", component: shift, name: "shift" },
+    { path: "/cashier", component: cashier, name: "cashier" }
 ];
-
 
 const router = new VueRouter({
     //mode: 'history',
-    routes:routes
+    routes: routes
 });
 
 router.beforeEach((to, from, next) => {
-    if ((to.name == 'profile') || (to.name == 'logout') || (to.name == 'manager') || (to.name == 'shift')) {
+    if (
+        to.name == "profile" ||
+        to.name == "logout" ||
+        to.name == "manager" ||
+        to.name == "shift"
+    ) {
         if (!store.state.user) {
-            next("/menu");
-            return; 
-        }       
-    }
-    if(to.name == 'manager'){
-        if(store.state.user.type != 'manager'){
             next("/menu");
             return;
         }
     }
-    if(to.name == 'login'){
-        if(store.state.user){
+    if (to.name == "manager") {
+        if (store.state.user.type != "manager") {
+            next("/menu");
+            return;
+        }
+    }
+    if (to.name == "login") {
+        if (store.state.user) {
             next("/menu");
             return;
         }
@@ -98,31 +152,30 @@ router.beforeEach((to, from, next) => {
     next();
 });
 
-
-
 const app = new Vue({
-	router,
+    router,
     store,
-    data:{
-    },
+    data: {},
     created() {
-        this.$store.commit('loadTokenAndUserFromSession');
+        this.$store.commit("loadTokenAndUserFromSession");
     },
-    methods:{
-
-    },
+    methods: {},
     sockets: {
-        connect(){
-            console.log('socket connected (socket ID = '+this.$socket.id+')');
-            if(this.$store.state.user !== null && this.$store.state.user.shift_active === 1){
-              this.$socket.emit('user_enter', store.state.user);
+        connect() {
+            console.log(
+                "socket connected (socket ID = " + this.$socket.id + ")"
+            );
+            if (
+                this.$store.state.user !== null &&
+                this.$store.state.user.shift_active === 1
+            ) {
+                this.$socket.emit("user_enter", store.state.user);
             }
         },
-        msg_from_server_managers(dataFromServer){
-            this.msgManagersTextArea = dataFromServer + '\n' + this.msgManagersTextArea;
-        },
+        msg_from_server_managers(dataFromServer) {
+            this.msgManagersTextArea =
+                dataFromServer + "\n" + this.msgManagersTextArea;
+        }
     },
-    mounted() {
-        
-    }
-}).$mount('#app');
+    mounted() {}
+}).$mount("#app");
